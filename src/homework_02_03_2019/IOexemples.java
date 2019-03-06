b192d193272f1db741ab513d613307855e75a929
aditonita <tonita.adi@gmail.com>
2019-03-06 08:51:50 +0200

    homework IO

---
+++ src/homework_02_03_2019/IOexemples.java
@@ -0,0 +1,31 @@
+package homework_02_03_2019;
+
+public class IOexemples {
+
+	public static void main(String[] args) {
+		/*
+		1. Write a program to get a list of all file/directory names from the given. 
+		2. Write a program to get specific files by extensions from a specified folder. 
+		3. Write a program to check if a file or directory specified by pathname exists or not. 
+		4. Write a program to check if a file or directory has read and write permission. 
+		5. Write a program to check if given pathname is a directory or a file. 
+		6. Write a program to compare two files lexicographically. 
+		7. Write a program to get last modified time of a file. 
+		8. Write program to read input from java console. 
+		9. Write a program to get file size in bytes, kb, mb. 
+		10. Write a program to read contents from a file into byte array. 
+		11. Write a program to read a file content line by line. 
+		12. Write a program to read a plain text file. 
+		13. Write a program to read a file line by line and store it into a variable. 
+		14. Write a program to store text file content line by line into an array. 
+		15. Write a program to write and read a plain text file. 
+		16. Write a program to append text to an existing file. 
+		17. Write a program to read first 3 lines from a file. 
+		18. Write a program to find the longest word in a text file. 
+		19. Write a program to extract all the words (only) from a text file and sort them by number of occurrences in the file.
+		*/
+		
+
+	}
+
+}
